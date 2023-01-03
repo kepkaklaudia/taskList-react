395d5973267b7ad468d1213a02f468147edab4da
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-03 18:11:09 +0100

    GlobalStyle added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    background-color: #f0f0f0;
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
