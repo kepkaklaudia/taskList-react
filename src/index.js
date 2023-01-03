c9e097f582f0583a323a3be28cde581a511f139a
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-03 18:13:15 +0100

    bug deleted

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from './GlobalStyle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
